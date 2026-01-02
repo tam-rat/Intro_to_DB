a9a33a01e4aca71355a6c63fd0d568c10c67c0a8
tam-rat <tamrathabtamu1@gmail.com>
2026-01-02 19:50:30 +0300

    1

---
+++ task_5.sql
@@ -0,0 +1,17 @@
+-- task_5.sql
+-- Insert a single record into the customers table
+
+USE alx_book_store;
+
+INSERT INTO customers (
+    customer_id,
+    customer_name,
+    email,
+    address
+)
+VALUES (
+    1,
+    'Cole Baidoo',
+    'cbaidoo@sandtech.com',
+    '123 Happiness Ave.'
+);
